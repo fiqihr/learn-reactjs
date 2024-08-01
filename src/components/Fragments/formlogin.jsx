1756b77c73bf3ff93289c526aac66c028decc692
Fiqih Ref <fiqihref15@gmail.com>
2024-08-01 20:08:53 +0700

    post data API

--- src/components/Fragments/formlogin.jsx
+++ src/components/Fragments/formlogin.jsx
@@ -1,30 +1,44 @@
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import Button from "../Elements/Button";
 import InputForm from "../Elements/Input";
+import { login } from "../../services/auth.service";
 
 const FormLogin = () => {
+  const [loginFailed, setLoginFailed] = useState("");
   const handleLogin = (event) => {
     // preventDefault berguna agar browser tidak melakukan refresh terus menerus
     event.preventDefault();
-    localStorage.setItem('email', event.target.email.value);
-    localStorage.setItem('password', event.target.password.value);
-    window.location.href = "/products";
+    // localStorage.setItem('email', event.target.email.value);
+    // localStorage.setItem('password', event.target.password.value);
+    
+    const data = {
+      username: event.target.username.value,
+      password: event.target.password.value,
+    };
+    login(data, (status, res) => {
+      if (status) {
+        localStorage.setItem("token", res);
+        window.location.href = "/products";
+      } else {
+        setLoginFailed(res.response.data);
+      }
+    });
   };
-  
-  const emailRef = useRef(null);
+  const usernameRef = useRef(null);
 
   useEffect(() => {
-    emailRef.current.focus();
+    usernameRef.current.focus();
   }, []);
 
   return (
     <form onSubmit={handleLogin}>
+      
       <InputForm
-        label="Email"
-        name="email"
-        type="email"
-        placeholder="Enter your email"
-        ref={emailRef}
+        label="Username"
+        name="username"
+        type="text"
+        placeholder="John Doe"
+        ref={usernameRef}
       />
       <InputForm
         label="Password"
@@ -33,6 +47,7 @@ const FormLogin = () => {
         placeholder="Enter your password"
       />
       <Button classname="bg-blue-600 w-full"  type="submit" >Login</Button>
+      {loginFailed && <p className="text-red-500 text-center mt-5">{loginFailed}</p>}
     </form>
   );
 };
