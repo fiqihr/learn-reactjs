390bb2601a98768faf9625e1cba43760eb89dc63
Fiqih Ref <fiqihref15@gmail.com>
2024-08-05 21:15:07 +0700

    useContext

--- src/Pages/products.jsx
+++ src/Pages/products.jsx
@@ -1,11 +1,13 @@
-import { Fragment, useEffect, useState } from "react";
+import { Fragment, useContext, useEffect, useState } from "react";
 import CardProduct from "../components/Fragments/CardProduct";
 import { getProducts } from "../services/product.service";
 import { useLogin } from "../hooks/useLogin";
 import TableCart from "../components/Fragments/TableCart";
 import Navbar from "../components/Layouts/Navbar";
+import { DarkMode } from "../context/DarkMode";
 
 const ProductsPage = () => {
+  const { isDarkMode } = useContext(DarkMode);
   const [products, setProducts] = useState([]);
   useLogin();
 
@@ -15,10 +17,11 @@ const ProductsPage = () => {
     });
   }, []);
 
+
   return (
     <Fragment>
-
-      <div className="flex justify-center py-5">
+    <Navbar/>
+      <div className={`flex justify-center py-5 ${isDarkMode && "bg-slate-900"}`}>
         <div className="w-4/6 flex flex-wrap">
           {products.length > 0 &&
             products.map((product) => (
